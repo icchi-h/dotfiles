fd5d01551920e42c0a26796e267aecbca9b322b8
Haruyuki Ichino <mail@icchi.me>
2020-01-31 03:14:50 +0900

    Add: fish shell files

---
+++ .config/fish/config.fish
@@ -0,0 +1,44 @@
+################################################################
+# icchi's fish shell setting
+################################################################
+
+################################################
+# PATH
+################################################
+# bin
+set PATH $HOME/bin $PATH
+
+# editor
+set EDITOR vim
+
+# node.js (nodebrew)
+set PATH $HOME/.nodebrew/current/bin $PATH
+
+# golang
+if type -q go # `go`コマンドが存在すれば
+    set GOROOT `go env GOROOT`
+    set GOPATH $HOME/Programs/go
+    set PATH $GOROOT/bin $GOPATH/bin $PATH
+end
+################################################
+
+################################################
+# Alias
+################################################
+# Mac
+alias o='open'
+
+# java
+alias javac='javac -J-Dfile.encoding=UTF-8'
+alias java='java -Dfile.encoding=UTF-8'
+
+# Tools
+alias typora='open -a typora'
+################################################
+
+################################################
+# Plugins
+################################################
+# fzf
+set -U FZF_LEGACY_KEYBINDINGS 0
+################################################
